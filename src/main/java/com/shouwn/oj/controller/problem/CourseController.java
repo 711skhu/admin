cc8b901b47debf64ba4fdda0937f9138be2d98ae
shouwn <sjonghyon@gmail.com>
2019-04-19 09:02:04 +0900

    강좌 생성 및 조회 구현

--- src/main/java/com/shouwn/oj/controller/problem/CourseController.java
+++ src/main/java/com/shouwn/oj/controller/problem/CourseController.java
@@ -1,7 +1,62 @@
 package com.shouwn.oj.controller.problem;
 
-import org.springframework.web.bind.annotation.RestController;
+import javax.persistence.EntityNotFoundException;
 
-@RestController
+import com.shouwn.oj.model.entity.problem.Course;
+import com.shouwn.oj.model.response.ApiResponse;
+import com.shouwn.oj.model.response.CommonResponse;
+import com.shouwn.oj.service.problem.CourseService;
+import com.shouwn.oj.service.problem.CourseServiceForAdmin;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriBuilder;
+
+@RestController("course")
 public class CourseController {
+
+	private final CourseService courseService;
+	private final CourseServiceForAdmin courseServiceForAdmin;
+
+	public CourseController(CourseService courseService,
+							CourseServiceForAdmin courseServiceForAdmin) {
+		this.courseService = courseService;
+		this.courseServiceForAdmin = courseServiceForAdmin;
+	}
+
+	@PostMapping
+	public ApiResponse<?> makeCourse(@RequestParam Long requesterId, // TODO 일반 adminId 가 아니라 Spring Security 에 의한 사용자 id가 필요
+									 @RequestParam String courseName,
+									 @RequestParam String courseDescription,
+									 UriBuilder uriBuilder) {
+
+		Course newCourse = courseServiceForAdmin.makeCourse(requesterId, courseName, courseDescription);
+
+		return CommonResponse.builder()
+				.status(HttpStatus.CREATED.value())
+				.message("강좌 생성 성공")
+				.data(uriBuilder.path("/course/{id}").build(newCourse.getId()))
+				.build();
+	}
+
+	@GetMapping("{courseId}")
+	public ApiResponse<?> getCourse(@PathVariable Long courseId) {
+
+		Course course;
+
+		try {
+			course = courseService.findCourseById(courseId);
+		} catch (EntityNotFoundException e) {
+			return CommonResponse.builder()
+					.status(HttpStatus.NOT_FOUND.value())
+					.message(courseId + "에 해당하는 강좌가 없습니다.")
+					.build();
+		}
+
+		return CommonResponse.builder()
+				.status(HttpStatus.OK.value())
+				.message("강좌 찾기 성공")
+				.data(course)
+				.build();
+	}
 }
--- src/main/java/com/shouwn/oj/service/problem/CourseServiceForAdmin.java
+++ src/main/java/com/shouwn/oj/service/problem/CourseServiceForAdmin.java
@@ -1,7 +1,41 @@
 package com.shouwn.oj.service.problem;
 
-import org.springframework.stereotype.Service;
+import javax.persistence.EntityNotFoundException;
 
+import com.shouwn.oj.model.entity.member.Admin;
+import com.shouwn.oj.model.entity.problem.Course;
+import com.shouwn.oj.repository.problem.CourseRepository;
+import com.shouwn.oj.service.member.AdminService;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
 @Service
 public class CourseServiceForAdmin {
+
+	private final AdminService adminService;
+
+	private final CourseRepository courseRepository;
+
+	public CourseServiceForAdmin(CourseRepository courseRepository,
+							   AdminService adminService) {
+		this.courseRepository = courseRepository;
+		this.adminService = adminService;
+	}
+
+	/**
+	 * admin Id 에 해당하는 관리자로 강좌를 생성하는 메소드
+	 *
+	 * @param adminId 강좌를 생성하는 admin 의 id
+	 * @param courseName 강좌 이름
+	 * @param courseDescription 강좌 설명
+	 * @return 생성된 Course 객체
+	 * @throws EntityNotFoundException adminId 로 Admin 을 찾을 수 없을 때 발생하는 예외
+	 */
+	public Course makeCourse(Long adminId, String courseName, String courseDescription) throws EntityNotFoundException {
+		Admin professor = adminService.findAdminById(adminId);
+
+		return professor.makeCourse(courseName, courseDescription);
+	}
 }
