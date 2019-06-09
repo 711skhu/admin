297984c3332216ee985d0dd041ccd47abbeaba18
shouwn <sjonghyon@gmail.com>
2019-06-09 19:35:49 +0900

    PR리뷰 - url 수정

--- src/main/java/com/shouwn/oj/controller/member/AdminAuthController.java
+++ src/main/java/com/shouwn/oj/controller/member/AdminAuthController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("student/auth")
+@RequestMapping("admin/auth")
 public class AdminAuthController {
 
 	private final JwtProvider jwtProvider;
